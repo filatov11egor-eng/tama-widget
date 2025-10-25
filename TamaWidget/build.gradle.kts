c209bd985b25eddac53faad54fc42252758ec853
filatov11egor-eng <filatov11egor@gmail.com>
2025-10-26 01:28:49 +0300

    Update build.gradle.kts

--- TamaWidget/build.gradle.kts
+++ TamaWidget/build.gradle.kts
@@ -1,4 +1,15 @@
+// Корневой build.gradle.kts
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    base
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
